c9b4630ba1ed2792ad022e5d25b197ff793c8540
Yuri Silva <ysm281@gmail.com>
2025-03-16 20:11:53 -0300

    insercion of axios

--- front/src/main.js
+++ front/src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import axios from 'axios';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+// Configurar Axios globalmente
+app.config.globalProperties.$axios = axios.create({
+  baseURL: 'http://localhost:8000/api', // URL do backend
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  }
+});
+
+app.mount('#app');
