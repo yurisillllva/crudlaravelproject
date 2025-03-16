c56908c99a5278cb29b02bc5b086987ee1cd77dd
Yuri Silva <ysm281@gmail.com>
2025-03-16 20:12:52 -0300

    render category

--- front/src/components/CategoryItem.vue
+++ front/src/components/CategoryItem.vue
@@ -2,18 +2,30 @@
   <li class="list-group-item">
     <div class="d-flex justify-content-between align-items-center">
       <div>
-        <button @click="isExpanded = !isExpanded" class="btn btn-sm mr-2">
-          {{ isExpanded ? '-' : '+' }}
+        <button 
+          @click="toggleChildren"
+          class="btn btn-sm mr-2"
+          v-if="hasChildren"
+        >
+          {{ showChildren ? '−' : '+' }}
         </button>
         {{ category.name }}
+        <small class="text-muted d-block">{{ category.description }}</small>
       </div>
       <div>
-        <button @click="$emit('edit', category)" class="btn btn-sm btn-warning">Editar</button>
-        <button @click="$emit('delete', category.id)" class="btn btn-sm btn-danger ml-2">Excluir</button>
+        <button @click="$emit('edit', category)" class="btn btn-sm btn-warning">
+          Editar
+        </button>
+        <button @click="$emit('delete', category.id)" class="btn btn-sm btn-danger ml-2">
+          Excluir
+        </button>
       </div>
     </div>
-    
-    <ul v-if="isExpanded && category.children" class="list-group mt-2 ml-4">
+
+    <ul 
+      v-if="showChildren && category.children"
+      class="list-group mt-2 ml-4"
+    >
       <category-item
         v-for="child in category.children"
         :key="child.id"
@@ -30,7 +42,17 @@ export default {
   props: ['category'],
   data() {
     return {
-      isExpanded: false
+      showChildren: false
+    }
+  },
+  computed: {
+    hasChildren() {
+      return this.category.children && this.category.children.length > 0
+    }
+  },
+  methods: {
+    toggleChildren() {
+      this.showChildren = !this.showChildren
     }
   }
 }
