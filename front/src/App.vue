722bba03b28c044f2267123ca2263ce529411508
Yuri Silva <ysm281@gmail.com>
2025-03-16 18:36:05 -0300

    call to component category manager

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,26 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mt-5">
+    <h1>Gerenciamento de Categorias</h1>
+    <category-manager></category-manager>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CategoryManager from './components/CategoryManager.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    CategoryManager
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
