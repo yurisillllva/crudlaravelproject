7c40e4702d0262d3b98a9fa9cf366c19d9058ca7
Yuri Silva <ysm281@gmail.com>
2025-03-16 20:09:13 -0300

    create controller

--- category-manager-api/app/Http/Controllers/Controller.php
+++ category-manager-api/app/Http/Controllers/Controller.php
@@ -6,29 +6,8 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use App\Models\Category;
-use App\Http\Resources\CategoryResource;
 
-class CategoryController extends Controller
+class Controller extends BaseController
 {
-    public function index()
-    {
-        $categories = Category::with('children')->whereNull('parent_id')->get();
-        return CategoryResource::collection($categories);
-    }
-
-    public function store(Request $request)
-    {
-        $validated = $request->validate([
-            'name' => 'required|string|max:255',
-            'description' => 'nullable|string',
-            'parent_id' => 'nullable|exists:categories,id'
-        ]);
-
-        $category = Category::create($validated);
-        return new CategoryResource($category);
-    }
-
-    //show, update e destroy...
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
-
