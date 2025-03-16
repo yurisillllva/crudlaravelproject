6471e80aada2bdf036a4c61aaf44aeaf372d1ef6
Yuri Silva <ysm281@gmail.com>
2025-03-16 18:33:26 -0300

    seeder create for categories added 5 categories

--- category-manager-api/database/seeders/DatabaseSeeder.php
+++ category-manager-api/database/seeders/DatabaseSeeder.php
@@ -12,7 +12,9 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-    }
+{
+    \App\Models\Category::factory()
+        ->count(5)
+        ->create();
+}
 }
