6e324a9b9090b4ec6774cc3f361cd546d81de826
Yuri Silva <ysm281@gmail.com>
2025-03-17 01:32:13 -0300

    added icons and css global

--- front/src/main.js
+++ front/src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import axios from 'axios';
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import '@/assets/css/pagination.css';
 
 // Importe apenas o CSS do Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'
