18ded3774f90a583300eda5d3f50b60c2b530624
Yuri Silva <ysm281@gmail.com>
2025-03-16 22:22:56 -0300

    component pagination

---
+++ front/src/components/PaginationComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="mt-4">
+    <nav aria-label="Page navigation">
+      <ul class="pagination justify-content-center">
+        <li 
+          v-for="(link, index) in paginationLinks" 
+          :key="index"
+          class="page-item"
+          :class="{ 
+            'active': link.active,
+            'disabled': !link.url
+          }"
+        >
+          <button
+            class="page-link"
+            @click="changePage(link.label)"
+            v-html="link.label"
+          ></button>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
